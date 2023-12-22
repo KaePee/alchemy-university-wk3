5823a3194d2e51dc867c4fd85a2e1490ec3528e7
Obed Somuah <obedpksomuah@gmail.com>
2023-12-22 00:18:41 +0000

    fix imports

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
   });
 
   return (<>
-  <div className="App">Block Number: {blockNumber}</div>
+  {/* <div className="App">Block Number: {blockNumber}</div> */}
   <Navbar/>
   <Search/>
   <Overview/>
