5759a8cd6fa83a133fe87d0d0dbc5cafbba56161
Obed Somuah <obedpksomuah@gmail.com>
2023-12-22 00:55:55 +0000

    structure added

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -2,7 +2,28 @@ import React from 'react'
 import "../styles/Overview.css"
 const Overview = () => {
   return (
-    <h1 className='overview'>Overview</h1>
+    <div>
+      <div> 
+        <h3>Current Etheruem Price</h3>
+        <span>ether price</span>
+        <h3>Market Cap</h3>
+        <span>market cap value</span>
+      </div>
+
+      <div>
+        <h3>Total Transactions</h3>
+        <span>Tx value</span>
+        <h3>Median Gas Price</h3>
+        <span>Gwei ($)</span>
+      </div>
+
+      <div>
+        <h3>Latest Finalized Block</h3>
+        <span>finalized Block number</span>
+        <h3>Latest Safe Block</h3>
+        <span>safe Block number</span>
+      </div>
+    </div>
   )
 }
 
